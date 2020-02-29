eb551436911c664c90f6f6ccb2a0530252e5116b
xiaoming8cn <54051256+xiaoming8cn@users.noreply.github.com>
2020-02-29 21:08:40 +1100

    Update change.js

--- change.js
+++ change.js
@@ -1,5 +1,9 @@
 $(function(){
-  $('li').css({
-    'background-color': '#000000',
+  $('li').on('click', function(e){
+    $('li span').remove();
+    var date = new Date();
+    date.setTime(e.timeStamp);
+    var clicked=date.toDateString();
+    $(this).append('<span class="date">' +clicked + ''+e.type + '</span>');
   });
 });
