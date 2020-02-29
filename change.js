719a31e8987eee03db24189cdb6798f5f72faeb2
xiaoming8cn <54051256+xiaoming8cn@users.noreply.github.com>
2020-02-29 20:43:00 +1100

    Update change.js

--- change.js
+++ change.js
@@ -1,6 +1,6 @@
 $(function(){
   $('li').each(function(){
     var ids = this.id;
-    $(this).append('<span class="hot">' + ids + '</span>');
+    $(this).append('<span class="order">' + ids + '</span>');
   });
 });
